142fbd9edb7730580733864d11f88c18b3f029b2
anuraags219 <anuraags219@gmail.com>
2026-06-10 14:10:33 +0530

    unused script

--- scripts/run_gluten.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker exec -u spark spark-local \
-/opt/spark/bin/spark-submit \
---properties-file /opt/spark/configs/gluten.conf \
---jars /opt/gluten/gluten-velox-bundle-spark3.5_2.12-linux_amd64-1.6.0.jar \
-"$1"
